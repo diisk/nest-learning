8d1bf82691ef805c2e0a676c529f630c9f86eeaf
diisk <diisk@hotmail.com>
2023-04-23 09:50:33 -0300

    Middleware finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
+import { logger} from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      // .exclude(
+      //   { path: 'cats', method: RequestMethod.GET },
+      //   { path: 'cats', method: RequestMethod.POST },
+      //   'cats/(.*)',
+      // )
+      .forRoutes(CatsController);
+      //.forRoutes({ path: 'cats', method: RequestMethod.GET });
+      //forRoutes({ path: 'ab*cd', method: RequestMethod.ALL });
+  }
+}
---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+
+// @Injectable()
+// export class LoggerMiddleware implements NestMiddleware {
+//   use(req: Request, res: Response, next: NextFunction) {
+//     console.log('Request...');
+//     next();
+//   }
+// }
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  console.log(`Request...`);
+  next();
+};
